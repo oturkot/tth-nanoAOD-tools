1e76721468d89f4054d6aabcebdebd60afdb621b
Ashraf Kasem <ashraf.mohamed@cern.ch>
2018-04-03 13:40:45 +0200

    Fix python/postprocessing/tthModules.py after merging

--- python/postprocessing/tthModules.py
+++ python/postprocessing/tthModules.py
@@ -12,7 +12,7 @@ from tthAnalysis.NanoAODTools.postprocessing.modules.eventCountHistogramProducer
 from tthAnalysis.NanoAODTools.postprocessing.modules.countHistogramProducer import countHistogramAll
 from tthAnalysis.NanoAODTools.postprocessing.modules.btagSFProducer_explicitBranchNames import btagSF_csvv2, btagSF_deep
 from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import puWeight
-<<<<<<< HEAD
+
 from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetUncertainties import jetmetUncertainties2017 as jetmetUncertainties17
 from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetUncertainties import jetmetUncertainties2016 as jetmetUncertainties16
 from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jecUncertainties import jecUncert
@@ -23,16 +23,9 @@ from tthAnalysis.NanoAODTools.postprocessing.modules.susy1lep_base import susy1l
 # it has  an issue 
 from tthAnalysis.NanoAODTools.postprocessing.modules.susy1lep_gen import genpartsusymod
 # for Trig
-from tthAnalysis.NanoAODTools.postprocessing.modules.susy1lep_trig import trigsusysusymod
+#from tthAnalysis.NanoAODTools.postprocessing.modules.susy1lep_trig import trigsusysusymod
 
-=======
-from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jecUncertainties import jecUncert_cpp
-from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetUncertainties import jetmetUncertainties2017 as jetmetUncertainties
-from tthAnalysis.NanoAODTools.postprocessing.modules.LT import LTModule
-from PhysicsTools.NanoAODTools.postprocessing.modules.jme.mht import mht
-<<<<<<< HEAD
->>>>>>> e149c13b0501cf8a5f102e58ef62209ac102beb2
-=======
+###### Oleksii
 from tthAnalysis.NanoAODTools.postprocessing.modules.susy_1l_triggers import susy_1l_Trigg
 from tthAnalysis.NanoAODTools.postprocessing.modules.susy_1l_filters import susy_1l_Filters
->>>>>>> 094e94777b5a99bf78db4f00a01be2bde8fdd5ce
+
